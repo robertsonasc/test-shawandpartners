ef22c0ed3f3991df4420ea0e48168ae93ed55cca
Robertson Nascimento <robertsonascimento99@gmail.com>
2022-12-24 17:57:29 -0300

    back-end configured

---
+++ back-end/index.js
@@ -0,0 +1,81 @@
+const app = require('express')();
+const PORT = 8080;
+
+const axios = require('axios');
+
+const instance = axios.create({
+  baseURL: 'https://api.github.com/users',
+});
+
+app.get('/api/users', async (req, res) => {
+  if (req.query.since) {
+    if (/^[0-9]+$/.test(req.query.since)) {
+      try {
+        const response = await instance.get(instance.defaults.baseURL, {
+          params: {
+            since: req.query.since,
+          }
+        })
+        let startIndex = response.headers.link.search('since=')
+        let endIndex = response.headers.link.search('>; rel="next"')
+        startIndex += 6
+
+        nextP = response.headers.link.toString().substring(startIndex, endIndex)
+        nextP = parseInt(nextP)
+
+        if (response.status === 200) {
+          res.status(200).send({
+            data: response.data,
+            nextPage: nextP
+          })
+        }
+      } catch (error) {
+        res.status(400).send(error)
+      }
+    } else {
+      res.status(400).send({
+        errorMessage: 'senci incorrect format'
+      })
+    }
+  } else {
+    res.status(400).send({
+      errorMessage: 'senci required'
+    })
+  }
+})
+
+app.get('/api/users/:username/details', async (req, res) => {
+  const { username } = req.params
+
+  try {
+    const response = await instance.get(`/${username}`)
+    if (response.status === 200) {
+      res.status(200).send(response.data)
+    }
+  } catch (error) {
+    res.status(400).send({
+      errorMessage: 'invalid username, or network error'
+    })
+  }
+})
+
+app.get('/api/users/:username/repos', async (req, res) => {
+  const { username } = req.params
+
+  try {
+    const response = await instance.get(`/${username}/repos`)
+    if (response.status === 200) {
+      res.status(200).send(response.data)
+    }
+  } catch (error) {
+    res.status(400).send({
+      errorMessage: 'invalid username, or network error'
+    })
+  }
+})
+
+app.listen(
+  PORT,
+  () => console.log(`back-end is live on http://localhost:${PORT}`)
+)
+
